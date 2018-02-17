1115834b4e849773552a7d7db148f750199d4654
Dan Hahne <giphahne@gmail.com>
2018-02-17 14:05:40 -0500

    adding setup.py and style config

--- py_cost_basis/cost_basis.py
+++ py_cost_basis/cost_basis.py
@@ -1,11 +1,9 @@
-
-
-
-
 def average_cost_basis(sells, buys):
     """
     """
     pass
+
+
 
 
 def lifo_cost_basis(sells, buys):
@@ -32,21 +30,18 @@ def low_cost_first_out_cost_basis(sells, buys):
     pass
 
 
-    
-
 def cost_basis(sells, buys, method="LIFO"):
     """
     """
-    
+
     try:
         return {
-            "average": average_cost_basis,        
+            "average": average_cost_basis,
             "LIFO": lifo_cost_basis,
             "FIFO": fifo_cost_basis,
             "HCFO": high_cost_first_out_cost_basis,
             "LCFO": low_cost_first_out_cost_basis,
         }[method](sells, buys)
-    
+
     except:
         raise
-    
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='py_cost_basis',
+    version='0.0.1',
+    description='computes cost basis',
+    author='Dan Hahne',
+    author_email='contact@danhahne.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: System Tools',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='taxes tax cost basis',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=[
+        'argcomplete',
+    ],
+    entry_points={
+        'console_scripts': [
+            'costbasis=py_cost_basis:main',
+        ],
+    },
+)
