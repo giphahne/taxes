8dcbceedc4571e115031ecc7cb881c361332cb1a
Dan Hahne <giphahne@gmail.com>
2018-02-24 16:57:30 -0500

    working on README

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# taxes
+Py Cost Basis
+=============
+
+
+Python package for computing cost basis of assets (such as securities or crypto-currencies).
+
+
+Justification
+--------------
+
+
+<stub>
+
+
+
+
+"Prior Art"/Literature Review
+------------------------------
+
+
+<stub>
--- py_cost_basis/data_ingest_tools.py
+++ py_cost_basis/data_ingest_tools.py
@@ -1,5 +1,6 @@
 import csv
 import json
+from datetime import datetime
 
 
 def base_load_transactions(*args):
@@ -15,15 +16,34 @@ def load_csv_file(csv_file):
     with open(csv_file, "r", encoding="utf-8") as csvfile:
         reader = csv.DictReader(
             (row for row in csvfile if not row.startswith("#"))
-        )  #,
-        #field_names)
+        )
         for row in reader:
-            yield row
+            for loader in (load_gdax_row, load_coinbase_row):
+                try:
+                    yield loader(row)
+                except Exception as e:
+                    continue
 
 
-def load_gdax_csv(csv_file):
-    pass
+def load_gdax_row(r):
+    return {
+        "product": r["product"].split("-")[0],
+        "event": r["side"],
+        "timestamp": r["created at"],
+        "size": r["size"],
+        "price": r["price"],
+        "fee": r["fee"],
+        "source_record": r,
+    }
 
 
-def load_coinbase_csv(csv_file):
-    pass
+def load_coinbase_row(r):
+    return {
+        "product": r["Currency"],
+        "event": r["side"],
+        "timestamp": r["created at"],
+        "size": r["size"],
+        "price": r["price"],
+        "fee": r["fee"],
+        "source_record": r,
+    }
